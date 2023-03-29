796aa8256e847af86b62c42fc2afcc11af42eeda
nishamohandas <128414352+nishamohandas@users.noreply.github.com>
2023-03-28 21:06:20 -0400

    my second commit

---
+++ src/trailPackage/TrialClass.java
@@ -0,0 +1,10 @@
+package trailPackage;
+
+public class TrialClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My name is Nisha");
+	}
+
+}
